49746ee87d907c070822fcaeb895225c30e41701
Adam Tovatt <sporedenstore@hotmail.com>
2025-04-07 15:39:14 +0200

    add tests for grid, refactor grid a bit

--- Sudoku/Grid.cs
+++ Sudoku/Grid.cs
@@ -1,93 +1,88 @@
 ﻿namespace Sudoku
 {
+    /// <summary>
+    /// Represents a Sudoku grid. Like the "board" of a Sudoku.
+    /// </summary>
     public class Grid
     {
-        private readonly int[,] grid;
-        public int SideLength { get; }
-        public int BlockWidth { get; }
-        public int BlockHeight { get; }
+        private readonly int[,] grid; // this is where we store the numbers on the board
 
-        public Grid(int blockWidth, int blockHeight)
+        /// <summary>
+        /// The length of one of the sides of the grid. Assumes all grids are squares for now.
+        /// </summary>
+        public int SideLength { get; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Grid"/>
+        /// </summary>
+        /// <param name="sideLength">Optional parameter for the side length. Default is 9.</param>
+        public Grid(int sideLength = 9) // let's default the side length to 9 since we only care about that now anyway
         {
-            BlockWidth = blockWidth;
-            BlockHeight = blockHeight;
-            SideLength = blockWidth * blockHeight;
+            SideLength = sideLength;
             grid = new int[SideLength, SideLength];
         }
 
-        public static Grid CreateFromString(string gridString)
+        /// <summary>
+        /// Will create an instance of <see cref="Grid"/> from the data in the provided string. Both "0" and "." work as empty tiles.
+        /// </summary>
+        /// <param name="gridString">The string with the data.</param>
+        /// <param name="sideLength">Optional parameter for the length of a side in the square grid. Defaults to 9.</param>
+        /// <returns>A new instance of <see cref="Grid"/></returns>
+        public static Grid CreateFromString(string gridString, int sideLength = 9)
         {
             throw new NotImplementedException($"{nameof(CreateFromString)} needs to be implemented before it can be used!");
         }
 
+        /// <summary>
+        /// Will return a bool indicating wether or not the grid that this method is called on is identical to the other grid in terms of the values that are in the cells of the grid.
+        /// </summary>
+        /// <param name="otherGrid">The other grid to compare to.</param>
         public bool HasSameCellValuesAs(Grid otherGrid)
         {
             throw new NotImplementedException($"{nameof(CreateFromString)} needs to be implemented before it can be used!");
         }
 
+        /// <summary>
+        /// Will get the value of a cell at a specific coordinate.
+        /// </summary>
+        /// <param name="x">The zero indexed x coordinate.</param>
+        /// <param name="y">The zero indexed y coordinate.</param>
+        /// <returns></returns>
         public int GetCell(int x, int y)
         {
             return grid[y, x];
         }
 
+        /// <summary>
+        /// Will set the value of a cell at a specific coordinate.
+        /// </summary>
+        /// <param name="x">The x coordinate to set the value for.</param>
+        /// <param name="y">The y coordinate to set the value for.</param>
+        /// <param name="value">The value to set at the given coordinates.</param>
         public void SetCell(int x, int y, int value)
         {
             if (value < 0 || value > SideLength)
             {
-                throw new ArgumentOutOfRangeException(nameof(value), $"Value must be between 0 and {SideLength} (0 means empty).");
+                throw new ArgumentOutOfRangeException(nameof(value), $"Value must be between 0 and 9 (0 means empty).");
             }
+
+            if(x < 0 || x >= SideLength)
+                throw new ArgumentOutOfRangeException(nameof(x), $"The index \"{x}\" is not valid. Must be between 0 and {SideLength}.");
+
+            if (y < 0 || y >= SideLength)
+                throw new ArgumentOutOfRangeException(nameof(y), $"The index \"{y}\" is not valid. Must be between 0 and {SideLength}.");
 
             grid[y, x] = value;
         }
 
+        /// <summary>
+        /// Will get a value indicating wether or not the cell at the provided coordinates is empty.
+        /// </summary>
+        /// <param name="x">The x coordinate to set the value for.</param>
+        /// <param name="y">The y coordinate to set the value for.</param>
         public bool IsCellEmpty(int x, int y)
         {
             return grid[y, x] == 0;
-        }
-
-        public void Clear()
-        {
-            for (int y = 0; y < SideLength; y++)
-            {
-                for (int x = 0; x < SideLength; x++)
-                {
-                    grid[y, x] = 0;
-                }
-            }
-        }
-
-        public static bool TryGetBlockDimensions(string input, out int blockWidth, out int blockHeight)
-        {
-            int length = input.Length;
-            int sideLength = 1;
-            int square = 1;
-
-            while (square < length)
-            {
-                sideLength++;
-                square = sideLength * sideLength;
-            }
-
-            if (square != length)
-            {
-                blockWidth = 0;
-                blockHeight = 0;
-                return false;
-            }
-
-            for (int w = 1; w <= sideLength; w++)
-            {
-                if (sideLength % w == 0)
-                {
-                    blockWidth = w;
-                    blockHeight = sideLength / w;
-                    return true;
-                }
-            }
-
-            blockWidth = 0;
-            blockHeight = 0;
-            return false;
         }
     }
 }
--- SudokuTests/GridTests.cs
+++ SudokuTests/GridTests.cs
@@ -6,26 +6,170 @@ namespace SudokuTests
     public class GridTests
     {
         [TestMethod]
-        public void GetSideLengthFromString1()
+        public void CreateGridFromString1()
         {
-            // 9x9 string
-            const string gridString = "900000002010060390083900100804095007130670049060041000302010050000500000541080030";
+            const string stringData = "1..5.37..6.3..8.9......98...1.......8761..........6...........7.8.9.76.47...6.312";
 
-            bool couldGetSideLength = Grid.TryGetBlockDimensions(gridString, out int blockWidth, out int blockHeight);
+            Grid grid = Grid.CreateFromString(stringData, 9);
 
-            Assert.IsTrue(couldGetSideLength);
+            Assert.AreEqual(9, grid.SideLength);
 
-            // 9x9 grids are created by 3x3 grids of 3x3 blocks, so the blocks should have side lengths that are 3
-            Assert.AreEqual(3, blockWidth);
-            Assert.AreEqual(3, blockHeight);
+            Assert.IsFalse(grid.IsCellEmpty(0, 0));
+            Assert.IsTrue(grid.IsCellEmpty(1, 0));
+
+            Assert.AreEqual(1, grid.GetCell(0, 0));
+            Assert.AreEqual(0, grid.GetCell(1, 0));
+            Assert.AreEqual(0, grid.GetCell(2, 0));
+            Assert.AreEqual(5, grid.GetCell(3, 0));
+            Assert.AreEqual(0, grid.GetCell(4, 0));
+            Assert.AreEqual(3, grid.GetCell(5, 0));
+            Assert.AreEqual(7, grid.GetCell(6, 0));
+            Assert.AreEqual(0, grid.GetCell(7, 0));
+            Assert.AreEqual(0, grid.GetCell(8, 0));
+
+            Assert.AreEqual(6, grid.GetCell(0, 1));
+            Assert.AreEqual(0, grid.GetCell(1, 1));
+            Assert.AreEqual(3, grid.GetCell(2, 1));
+            Assert.AreEqual(0, grid.GetCell(3, 1));
+            Assert.AreEqual(0, grid.GetCell(4, 1));
+            Assert.AreEqual(8, grid.GetCell(5, 1));
+            Assert.AreEqual(0, grid.GetCell(6, 1));
+            Assert.AreEqual(9, grid.GetCell(7, 1));
+            Assert.AreEqual(0, grid.GetCell(8, 1));
+
+            Assert.AreEqual(0, grid.GetCell(0, 2));
+            Assert.AreEqual(0, grid.GetCell(1, 2));
+            Assert.AreEqual(0, grid.GetCell(2, 2));
+            Assert.AreEqual(9, grid.GetCell(3, 2));
+            Assert.AreEqual(8, grid.GetCell(4, 2));
+            Assert.AreEqual(0, grid.GetCell(5, 2));
+            Assert.AreEqual(0, grid.GetCell(6, 2));
+            Assert.AreEqual(0, grid.GetCell(7, 2));
+            Assert.AreEqual(1, grid.GetCell(8, 2));
+
+            Assert.AreEqual(0, grid.GetCell(5, 0));
+            Assert.AreEqual(1, grid.GetCell(6, 3));
+            Assert.AreEqual(2, grid.GetCell(7, 1));
+            Assert.AreEqual(3, grid.GetCell(8, 2));
         }
 
         [TestMethod]
-        public void CreateGridFromString1()
+        public void CreateGridFromString2()
         {
-            const string gridString = "900000002010060390083900100804095007130670049060041000302010050000500000541080030";
+            const string stringData = "..3.8.5...4.....9.7..15.4..98...1..5..2.7983...5.6....1....6.5.8.....7..........4";
 
-            Grid grid = Grid.CreateFromString(gridString);
+            Grid grid = Grid.CreateFromString(stringData, 9);
+
+            Assert.AreEqual(9, grid.SideLength);
+
+            Assert.IsTrue(grid.IsCellEmpty(0, 0));
+            Assert.IsTrue(grid.IsCellEmpty(1, 0));
+            Assert.AreEqual(3, grid.GetCell(2, 0));
+            Assert.AreEqual(8, grid.GetCell(4, 0));
+            Assert.AreEqual(5, grid.GetCell(6, 0));
+
+            Assert.AreEqual(4, grid.GetCell(0, 1));
+            Assert.IsTrue(grid.IsCellEmpty(1, 1));
+            Assert.IsTrue(grid.IsCellEmpty(2, 1));
+            Assert.IsTrue(grid.IsCellEmpty(3, 1));
+            Assert.IsTrue(grid.IsCellEmpty(4, 1));
+            Assert.AreEqual(9, grid.GetCell(5, 1));
+            Assert.AreEqual(7, grid.GetCell(6, 1));
+
+            Assert.AreEqual(1, grid.GetCell(0, 2));
+            Assert.AreEqual(5, grid.GetCell(2, 2));
+            Assert.AreEqual(4, grid.GetCell(4, 2));
+            Assert.IsTrue(grid.IsCellEmpty(6, 2));
+
+            Assert.AreEqual(9, grid.GetCell(0, 3));
+            Assert.AreEqual(8, grid.GetCell(1, 3));
+            Assert.IsTrue(grid.IsCellEmpty(2, 3));
+            Assert.IsTrue(grid.IsCellEmpty(3, 3));
+            Assert.AreEqual(1, grid.GetCell(6, 3));
+
+            Assert.AreEqual(2, grid.GetCell(2, 4));
+            Assert.AreEqual(7, grid.GetCell(4, 4));
+            Assert.AreEqual(9, grid.GetCell(5, 4));
+            Assert.AreEqual(8, grid.GetCell(6, 4));
+            Assert.AreEqual(3, grid.GetCell(7, 4));
+
+            Assert.AreEqual(5, grid.GetCell(3, 5));
+            Assert.AreEqual(6, grid.GetCell(5, 5));
+            Assert.AreEqual(1, grid.GetCell(6, 5));
+
+            Assert.AreEqual(6, grid.GetCell(4, 6));
+            Assert.AreEqual(5, grid.GetCell(6, 6));
+            Assert.AreEqual(8, grid.GetCell(8, 6));
+
+            Assert.AreEqual(7, grid.GetCell(5, 7));
+
+            Assert.AreEqual(4, grid.GetCell(8, 8));
+        }
+
+        [TestMethod]
+        public void CompareTwoGrids1()
+        {
+            const string gridData = ".9.....8.5......96......4..6..34....9......2.2...6..17.1....8...6..17..97...95...";
+
+            Grid grid1 = Grid.CreateFromString(gridData, 9);
+            Grid grid2 = Grid.CreateFromString(gridData, 9);
+
+            Assert.IsTrue(grid1.HasSameCellValuesAs(grid2));
+
+            grid1 = Grid.CreateFromString(gridData, 9);
+            grid2 = Grid.CreateFromString(gridData); // create with out optional parameter here just to ensure the default of 9 is still working
+
+            Assert.IsTrue(grid1.HasSameCellValuesAs(grid2));
+        }
+
+        [TestMethod]
+        public void CompareTwoGrids2()
+        {
+            const string gridData = "..7.....34...6..12.....37..1.8.57.......8..6......21...........6...459....9....87";
+
+            Grid grid1 = Grid.CreateFromString(gridData, 9);
+            Grid grid2 = Grid.CreateFromString(gridData, 9);
+
+            Assert.IsTrue(grid1.HasSameCellValuesAs(grid2));
+
+            grid1 = Grid.CreateFromString(gridData, 9);
+            grid2 = Grid.CreateFromString(gridData); // create with out optional parameter here just to ensure the default of 9 is still working
+
+            Assert.IsTrue(grid1.HasSameCellValuesAs(grid2));
+        }
+
+        [TestMethod]
+        public void CompareTwoGrids3()
+        {
+            const string gridData1 = ".9.....8.5......96......4..6..34....9......2.2...6..17.1....8...6..17..97...95...";
+            const string gridData2 = "..7.....34...6..12.....37..1.8.57.......8..6......21...........6...459....9....87";
+
+            Grid grid1 = Grid.CreateFromString(gridData1, 9);
+            Grid grid2 = Grid.CreateFromString(gridData2, 9);
+
+            Assert.IsFalse(grid1.HasSameCellValuesAs(grid2));
+
+            grid1 = Grid.CreateFromString(gridData1, 9);
+            grid2 = Grid.CreateFromString(gridData2); // create with out optional parameter here just to ensure the default of 9 is still working
+
+            Assert.IsFalse(grid1.HasSameCellValuesAs(grid2));
+        }
+
+        [TestMethod]
+        public void CompareTwoGrids4()
+        {
+            const string gridData1 = ".9.....8.5......96......4..6..34....9......2.2...6..17.1....8...6..17..97...95...";
+            const string gridData2 = ".9.....8.5......96......4..6..34....91.....2.2...6..17.1....8...6..17..97...95..."; // just one character differing
+
+            Grid grid1 = Grid.CreateFromString(gridData1, 9);
+            Grid grid2 = Grid.CreateFromString(gridData2, 9);
+
+            Assert.IsFalse(grid1.HasSameCellValuesAs(grid2));
+
+            grid1 = Grid.CreateFromString(gridData1, 9);
+            grid2 = Grid.CreateFromString(gridData2); // create with out optional parameter here just to ensure the default of 9 is still working
+
+            Assert.IsFalse(grid1.HasSameCellValuesAs(grid2));
         }
     }
 }
